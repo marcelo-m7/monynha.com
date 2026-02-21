b7423414b872bbd2fccca1b78852b65852bbe381
Marcelo Santos <marcelo.net77@hotmail.com>
2026-02-21 11:08:04 +0000

    refactor: setup for v4

--- src/components/Navigation.tsx
+++
@@ -1,7 +0,0 @@
-import { GooeyNav } from "@/components/reactbits/GooeyNav";
-
-export const Navigation = () => {
-  return <GooeyNav />;
-};
-
-Navigation.displayName = "Navigation";
--- src/vitest.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-
-declare module "vitest" {
-  interface Assertion<T = HTMLElement> extends TestingLibraryMatchers<T, void> {}
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<HTMLElement, void> {}
-}
